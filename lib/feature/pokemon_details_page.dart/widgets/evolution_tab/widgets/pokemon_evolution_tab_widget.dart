e9b876b8ae2620e90298080531609ae7f4570220
Enrico Luis M. Cuison <61441278+Enrickzz@users.noreply.github.com>
2022-08-01 17:06:20 +0800

    PR Feedback
    -evolution_tab clean up (DRY code issue)
    -traling commas

--- lib/feature/pokemon_details_page.dart/widgets/evolution_tab/widgets/pokemon_evolution_tab_widget.dart
+++ lib/feature/pokemon_details_page.dart/widgets/evolution_tab/widgets/pokemon_evolution_tab_widget.dart
@@ -4,32 +4,53 @@ import 'package:pokedex_async_redux/utilities/constants.dart';
 import 'package:pokedex_async_redux/utilities/extensions.dart';
 
 class PokemonEvolutionWidget extends StatelessWidget {
-  const PokemonEvolutionWidget({this.thisSpecies});
-  final Species? thisSpecies;
+  const PokemonEvolutionWidget({
+    required this.thisSpecies,
+    required this.arrowCheck,
+  });
+  final List<Species> thisSpecies;
+  final bool arrowCheck;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      width: 80,
-      child: Stack(
-        children: [
-          Image.network(
-            '$pokemonImgUrl${thisSpecies?.id ?? ''}.png',
-            width: 50,
-            height: 50,
-          ),
-          Positioned(
-            bottom: 15,
-            left: 5,
-            child: Text('#${thisSpecies?.id.toString().padLeft(3, '0') ?? ''}'),
-          ),
-          Positioned(
-            bottom: 2,
-            left: 5,
-            child: Text('${thisSpecies?.name.toString().capitalize ?? ''}'),
+    return Column(
+      children: thisSpecies
+          .map(
+            (thisSpecies) => Container(
+              margin: arrowCheck ? EdgeInsets.fromLTRB(30, 0, 0, 0) : EdgeInsets.all(0),
+              height: 80,
+              width: 80,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  if (arrowCheck)
+                    Positioned(
+                      left: -55,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        child: Icon(Icons.arrow_right_outlined),
+                      ),
+                    ),
+                  Image.network(
+                    '$pokemonImgUrl${thisSpecies.id ?? ''}.png',
+                    width: 50,
+                    height: 50,
+                  ),
+                  Positioned(
+                    bottom: 15,
+                    left: 5,
+                    child: Text('#${thisSpecies.id.toString().padLeft(3, '0')}'),
+                  ),
+                  Positioned(
+                    bottom: 2,
+                    left: 5,
+                    child: Text('${thisSpecies.name.toString().capitalize}'),
+                  )
+                ],
+              ),
+            ),
           )
-        ],
-      ),
+          .toList(),
     );
   }
 }

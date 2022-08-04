0f00613ce5e0e072dfee4d076e7151aff14dfdb6
Enrico Luis M. Cuison <61441278+Enrickzz@users.noreply.github.com>
2022-08-04 13:20:49 +0800

    PR Feedback
    -Search behavior improvements

--- lib/feature/home_page/widgets/pokemon_search_page.dart
+++ lib/feature/home_page/widgets/pokemon_search_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_async_redux/api/models/pokemon_model.dart';
 import 'package:pokedex_async_redux/feature/home_page/widgets/pokemon_tile.dart';
-import 'package:pokedex_async_redux/utilities/extensions.dart';
 
 class PokemonSearchPage extends SearchDelegate {
   PokemonSearchPage({required this.pokemonList});
@@ -18,33 +17,41 @@ class PokemonSearchPage extends SearchDelegate {
   Widget buildResults(BuildContext context) => Padding(
         padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
         child: GridView.count(
-            crossAxisCount: 2,
-            mainAxisSpacing: 5,
-            crossAxisSpacing: 5,
-            childAspectRatio: 3 / 2,
-            children: pokemonList
-                .where((element) => element.name!.startsWith(query))
-                .map((queried) => PokemonTile(
-                      thisPokemon: queried,
-                      key: UniqueKey(),
-                    ))
-                .toList()),
+          crossAxisCount: 2,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+          childAspectRatio: 3 / 2,
+          children: pokemonList
+              .where((element) => element.name!.startsWith(query))
+              .map((queried) => PokemonTile(
+                    thisPokemon: queried,
+                    key: UniqueKey(),
+                  ))
+              .toList(),
+        ),
       );
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    var suggestions = pokemonList.where((element) => element.name!.contains(query)).map((queried) => queried).toList();
-    return ListView.builder(
-      itemCount: suggestions.length,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(suggestions[index].name!.capitalize),
-          onTap: () {
-            query = suggestions[index].name!;
-            showResults(context);
-          },
-        );
-      },
+    var suggestions = <Pokemon>[];
+    if (query.isNotEmpty) {
+      suggestions = pokemonList.where((element) => element.name!.contains(query)).map((queried) => queried).toList();
+    }
+    return Padding(
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+      child: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+        childAspectRatio: 3 / 2,
+        children: suggestions
+            .where((element) => element.name!.contains(query))
+            .map((queried) => PokemonTile(
+                  thisPokemon: queried,
+                  key: UniqueKey(),
+                ))
+            .toList(),
+      ),
     );
   }
 }
